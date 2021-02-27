f19ec4e2e9691720cad7190a8d84cfdb9083403e
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-27 11:35:58 -0500

    math practice

--- practice-js-if-then/js/math.js
+++ practice-js-if-then/js/math.js
@@ -1,16 +1,24 @@
 // declare program variables
+let num1;
+let num2 ;
+let message ;
 
 // announce the program
 alert("Let's do some math!");
 
 // collect numeric input
-var num1 = prompt("Please type a number");
-num1 = parseFloat(num1);
-var num2 = prompt("Please type another number");
+num1 = prompt("Please type a number");
+num1 = parseFloat(num1)
+num2 = prompt("Please type another number");
 num2 = parseFloat(num2);
 
-// build an HTML message
-var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+if (num2 === 0) {
+    alert(`The second number is 0. You can't divide by zero. Reload and try again." `);
+} else if (isNaN(num1) || isNaN(num2) ) {
+    alert("At least one of the values you types is not a number. Reload and try again.");
+} else {
+ // build an HTML message
+message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
 message += num1 + " + " + num2 + " = " + (num1 + num2);
 message += "<br>";
 message += num1 + " * " + num2 + " = " + (num1 * num2);
@@ -21,4 +29,7 @@ message += num1 + " - " + num2 + " = " + (num1 - num2);
   
 // write message to web page
 document.write(message);
+}
+
+
 
