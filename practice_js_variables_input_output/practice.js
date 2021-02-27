a4af6a084f68fbb827f85838c151de6953d20bab
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-26 20:41:19 -0500

    template literals practice

--- practice_js_variables_input_output/practice.js
+++ practice_js_variables_input_output/practice.js
@@ -7,11 +7,13 @@ let wrd1 = prompt( "Please enter your first word");
 let wrd2 = prompt( "Please enter your second word");
 // 4. Create a third variable and which combines an uppercase version values in the two other variables separated by a space. For example, if the first two variables contain "sally" and "forth", this third variable should contain the string value "SALLY FORTH"
 
-let wrd3 = `${toUpperCase(wrd1)} ${toUpperCase(wrd2)}`;
+let wrd3 = `${wrd1.toUpperCase()} ${wrd2.toUpperCase()}`;
 console.log(wrd3);
 // 5. Create a fourth variable to store a number. The number should be the total number of characters in the third variable. 
-
+let num = `${wrd3.length}`;
 // 6. Add an alert dialog box that says "The string '[insert value of third variable here]' is X number of characters long." For example, if the third variable contained the string "SALLY FORTH" then the alert dialog should says "The string 'SALLY FORTH' is 11 characters long."
+
+alert(`The string ${wrd3} is ${num} characters long.`);
 
 
 
