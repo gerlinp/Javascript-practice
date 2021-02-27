31c32da346d4a9fcf2cfd836fb31af737ecf9ad6
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-27 13:27:54 -0500

    starter files and convert

---
+++ practice-arrow-functions-in-javascript/Practice Arrow Functions in JavaScript/1- Introducing the Practice/convert.js
@@ -0,0 +1,9 @@
+const addToTen = num => 10 + num;
+
+const divideUs = (num1, num2) => num1/num2;
+
+
+const printMyName = () => {
+  let myName = 'Ashley';
+	console.log(myName);
+}
---
+++ practice-arrow-functions-in-javascript/Practice Arrow Functions in JavaScript/4- Debug Arrow Function Expressions/convert.js
@@ -0,0 +1,8 @@
+const addToTen = num => 10 + num;
+
+const divideUs = (num1, num2) => num1/num2;
+
+const printMyName = () => {
+  let myName = 'Ashley';
+	console.log(myName);
+}
