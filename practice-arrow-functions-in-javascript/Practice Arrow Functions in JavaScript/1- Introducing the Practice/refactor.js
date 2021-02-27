2196c2e0fbe99a1668a334e56f924b3768aa68d0
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-27 13:33:50 -0500

    Update refactor.js

--- practice-arrow-functions-in-javascript/Practice Arrow Functions in JavaScript/1- Introducing the Practice/refactor.js
+++ practice-arrow-functions-in-javascript/Practice Arrow Functions in JavaScript/1- Introducing the Practice/refactor.js
@@ -1,13 +1,9 @@
-const multiplyByTen = (num) => {
-	return 10 * num;
-}
+const multiplyByTen = num => 10 * num;
 
-const printToTen = () => {
-	for (let i = 1; i <= 10; i++) {
+const printToTen = () => 
+	{ for (let i = 1; i <= 10; i++) {
 		console.log(i);
 	}
 }
 
-const addUs = (num1, num2) => {
-	console.log(num1 + num2);
-}
+const addUs = (num1, num2) => console.log(num1 + num2);
